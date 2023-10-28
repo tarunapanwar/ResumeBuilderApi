1813c29e181928c251349543f46e7cbc1187ee84
taruna panwar <tarunapanwar1996@gmail.com>
2023-10-28 20:26:33 +0530

    changes

--- src/database/conn.js
+++ src/database/conn.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const connectDb = async() => {
     try{
         mongoose.set('strictQuery', true);
-        await mongoose.connect(process.env.MONGO_LOCAL_URI);
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('mongo db is connected...');
     }catch(err){
         console.log('mongo db is not connected', err.message);
